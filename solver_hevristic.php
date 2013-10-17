53c963d28f7d22720228c745a087f4e62c2df581
zipper2110 <leetvin@gmail.com>
2013-10-17 15:36:13 +0600

    Switched to hevristic solver, that calls test prolog goal

--- solver_hevristic.php
+++ solver_hevristic.php
@@ -1,7 +1,4 @@
 <?php
-//echo exec('"C:\\Program Files\\swipl\\bin\\swipl.exe" -s \"d:\\1.pl" -g test -t halt', $output);
-//`C:/Program Files/7-Zip/7zFM.exe`;
-//echo `"C:/Program Files/7-Zip/7z.exe" -version`;
 
 $test = `"C:/Program Files/swipl/bin/swipl.exe" -s d:/1.pl -g test -t halt`;
 echo $test;
